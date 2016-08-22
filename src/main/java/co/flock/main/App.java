986e969856a1c13b37095239b79d313346488b74
hvadehra <hvadehra@gmail.com>
2016-08-23 00:39:06 +0530

    adding main class

--- src/main/java/co/flock/main/App.java
+++ src/main/java/co/flock/main/App.java
@@ -1,13 +1,30 @@
 package co.flock.main;
 
+import com.google.common.base.Stopwatch;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.util.Modules;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class.getCanonicalName());
+
+    private static void start(Module... modules) {
+        logger.warn("Starting server...");
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        Injector injector = Guice.createInjector(Modules.override(new ApplicationModule()).with(modules));
+        long startUpTime = stopwatch.elapsed(TimeUnit.SECONDS);
+        logger.warn("Server started in {} seconds", startUpTime);
+    }
+
+    public static void main(String[] args) {
+        start();
     }
 }
