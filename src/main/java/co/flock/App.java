4af49b8168659f92cbe916712f163b14928a3ae8
hvadehra <hvadehra@gmail.com>
2016-08-23 00:20:59 +0530

    Initial commit

---
+++ src/main/java/co/flock/App.java
@@ -0,0 +1,13 @@
+package co.flock;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
